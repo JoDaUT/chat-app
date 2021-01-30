e190318ab20c2ce2a8cddbb5f9cb44f44c94be0b
jodaut <josedavidut64@gmail.com>
2021-01-30 12:51:47 -0600

    prod config

--- client/chat-app-client/src/environments/environment.prod.ts
+++ client/chat-app-client/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyAqNdZSmMQrk3Sw5SZe83e12ipLzgFLl1w",
+    authDomain: "chat-app-ac2fe.firebaseapp.com",
+    projectId: "chat-app-ac2fe",
+    storageBucket: "chat-app-ac2fe.appspot.com",
+    messagingSenderId: "380513388976",
+    appId: "1:380513388976:web:ee447c53e920915492ae75",
+    measurementId: "G-GLPKQJXCWG"
+  }
 };
