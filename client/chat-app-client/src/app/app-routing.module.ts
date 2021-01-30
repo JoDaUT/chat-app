c6544dca62dc3a9f3675baec02fbdc1a42ad1b86
jodaut <josedavidut64@gmail.com>
2021-01-30 12:35:41 -0600

    route guards with firebase

--- client/chat-app-client/src/app/app-routing.module.ts
+++ client/chat-app-client/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import {LoginComponent} from './pages/login/login.component'
 import {ChatAppComponent} from './pages/chat-app/chat-app.component'
+
+import { AngularFireAuthGuard, redirectUnauthorizedTo,redirectLoggedInTo} from '@angular/fire/auth-guard';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
+const redirectLoggedInToChat = () => redirectLoggedInTo(['chat']);
+
 const routes: Routes = [
-  {path:"login", redirectTo:'/LoginComponent'},
-  {path:"chat", component:ChatAppComponent},
+  {path:"", component:LoginComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectLoggedInToChat }},
+  {path:"login", redirectTo:'/LoginComponent', canActivate:[AngularFireAuthGuard], data:{authGuardPipe: redirectLoggedInToChat}},
+  {path:"chat", component:ChatAppComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin }},
   {path:"**", component:LoginComponent}
 ];
 
