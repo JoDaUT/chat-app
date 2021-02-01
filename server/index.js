ef391fd245b449d2b5d6adcf7470f54975e6ac23
jodaut <josedavidut64@gmail.com>
2021-02-01 15:37:29 -0600

    typing feature and online feature, contact list

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
+'use strict'
+
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 const users= new Map();// key: id value: {props: nickname,etc}
+
 
 app.get('/', (req, res)=>{
     res.sendFile(__dirname+'/public/index.html')
@@ -10,12 +13,13 @@ app.get('/', (req, res)=>{
 });
 io.on('connection', (socket)=>{
     console.log('user conected '+socket.id);
-    // socket.broadcast.emit('user connect', socket.id);
+   // socket.broadcast.emit('user connect', socket.id);
 
     socket.on('nickname', nickname=>{
-        //console.log(`${nickname}: ${socket.id}`);
         users.set(socket.id, {id:socket.id, nickname});
         socket.broadcast.emit('user connect', users.get(socket.id));
+        //socket.broadcast.emit('get users', Array.from(users));
+        socket.emit('get users', Array.from(users));
     })
     socket.on('disconnect', ()=>{
         console.log('user disconnected '+socket.id);
@@ -29,6 +33,12 @@ io.on('connection', (socket)=>{
         console.log(msg);
         // console.log('message: '+users.get(socket.id)?nickname+":"+msg);
     })
+    socket.on('typing', ()=>{
+        socket.broadcast.emit('is typing', users.get(socket.id));
+    })
+    // socket.on('online', ()=>{
+    //     socket.broadcast.emit('is typing', users.get(socket.id));
+    // })
 })
 http.listen(3000, ()=>{
     console.log('listening on http://localhost:3000');
